5125a2a2d053b2f3494deef1262315829efc853a
heyolaniran <olaniran.abd@gmail.com>
2023-08-30 17:25:22 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client, NoAuth } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy : new NoAuth() 
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+
+	client.on('message', message => {
+        if(message.body === 'Bonjour') {
+            return client.sendMessage(message.from, 'Bonjour, en quoi pouvons nous vous aider ? ');
+        }
+
+        return client.sendMessage(message.from, 'Bonjour, en quoi pouvons nous vous aider ? ');
+    });
+
+// client initalisation
+client.initialize();
+
+
+    
+
+
+ 
+
+ 
+
+ 
+
+ 

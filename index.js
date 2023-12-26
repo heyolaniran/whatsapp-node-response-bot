b51a43954847920a67ef1daf33064d4bfc923eb5
heyolaniran <olaniran.abd@gmail.com>
2023-12-26 17:02:53 +0100

    feat: some package to switch to express app with ejs engine

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const qrcode = require('qrcode-terminal');
+var express = require('express') ; 
+var app  = express() ; 
+const qrcode = require('qrcode');
+app.set('view engine', 'ejs'); 
 
 const { Client, NoAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy : new NoAuth() 
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+app.get('/', function(req, res) {
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
+    client.on('qr', qr => {
+        qrcode.toDataURL(qr, (err, url) => {
+            res.render('pages/index', {
+                connect : url
+            })
+        });
+    });
 
 
-	client.on('message', message => {
+    client.on('ready', () => {
+        console.log('Client is ready!');
+    });
+    
+    
+    client.on('message', message => {
         if(message.body === 'Bonjour') {
             return client.sendMessage(message.from, 'Bonjour, en quoi pouvons nous vous aider ? ');
         }
-
+    
         return client.sendMessage(message.from, 'Bonjour, en quoi pouvons nous vous aider ? ');
     });
 
-// client initalisation
-client.initialize();
+    // client initalisation
+    client.initialize();
+
+})
+
+
+
+
+
+
+
+
+
+
 
 
     
